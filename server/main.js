225ccc6fe39eeb403a8981790cf6d7b1d31c0dce
redpolarbear <matthewxu@outlook.com>
2017-03-23 20:28:13 -0700

    http testing

--- server/main.js
+++ server/main.js
@@ -1,5 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  // try {
+    HTTP.call('GET', 'https://jsonplaceholder.typicode.com/pos', {},
+      function(error, response) {
+        if (error) {
+          throw new Meteor.Error(400, e.message);
+        } else {
+          console.log('response', response);
+        }
+      }
+    );  
+  // } catch(e) {
+  //   throw new Error('http error');
+  // }
+  
 });
